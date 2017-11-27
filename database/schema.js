718443b1d656bd68c895bd063df3f395974a316a
SaschaPWittwer <sascha.wittwer@bluewin.ch>
2017-11-27 23:29:24 +0100

    add lock file and remove user and session temporary

--- database/schema.js
+++ database/schema.js
@@ -1,5 +1,5 @@
 var Sequelize = require("sequelize");
-var sequelize = new Sequelize(process.env.GETSUB_MYSQL_DB, process.env.GETSUB_MYSQL_USER,process.env.GETSUB_MYSQL_PW, {
+var sequelize = new Sequelize(process.env.GETSUB_MYSQL_DB, process.env.GETSUB_MYSQL_USER, process.env.GETSUB_MYSQL_PW, {
     host: process.env.GETSUB_MYSQL_HOST,
     dialect: 'sqlite',
     // SQLite only
@@ -9,7 +9,8 @@ var sequelize = new Sequelize(process.env.GETSUB_MYSQL_DB, process.env.GETSUB_MY
 var Directory = sequelize.define("directory", {
     name: Sequelize.STRING,
     path: Sequelize.STRING,
-    lastSync: Sequelize.DATE
+    lastSync: Sequelize.DATE,
+    watch: Sequelize.BOOLEAN
 });
 
 var Video = sequelize.define("video", {
@@ -18,23 +19,23 @@ var Video = sequelize.define("video", {
     hasSubs: Sequelize.BOOLEAN
 });
 
-var User = sequelize.define("user", {
-    name: Sequelize.STRING,
-    password: Sequelize.STRING
-})
+// var User = sequelize.define("user", {
+//     name: Sequelize.STRING,
+//     password: Sequelize.STRING
+// })
 
-var Session = sequelize.define("session", {
-    token: Sequelize.STRING
-})
+// var Session = sequelize.define("session", {
+//     token: Sequelize.STRING
+// })
 
-Session.belongsTo(User);
+// Session.belongsTo(User);
 
 Video.belongsTo(Directory);
 
 exports.Directory = Directory;
 exports.Video = Video;
-exports.User = User;
-exports.Session = Session;
+// exports.User = User;
+// exports.Session = Session;
 exports.bootstrap = function() {
     sequelize.sync({ force: true });
 }
